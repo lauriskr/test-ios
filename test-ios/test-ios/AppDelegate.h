4060151139318e7f53e67d035de6a52c21e98642
Lauris Kruusamae <lauris.kruusamae@lab.mobi>
2014-04-24 22:27:54 +0300

    initial commit

---
+++ test-ios/test-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test-ios
+//
+//  Created by lauris on 4/24/14.
+//  Copyright (c) 2014 greenhouse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test-ios/test-iosTests/test_iosTests.m
@@ -0,0 +1,34 @@
+//
+//  test_iosTests.m
+//  test-iosTests
+//
+//  Created by lauris on 4/24/14.
+//  Copyright (c) 2014 greenhouse. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface test_iosTests : XCTestCase
+
+@end
+
+@implementation test_iosTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
